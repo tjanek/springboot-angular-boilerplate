f95deeac731c53e80bd00eff275ad672ea168ff5
Tomasz Janek <janek.tomasz@gmail.com>
2018-01-30 21:08:09 +0100

    Initial commit

---
+++ src/main/java/pl/tjanek/boilerplate/BoilerplateApplication.java
@@ -0,0 +1,12 @@
+package pl.tjanek.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateApplication.class, args);
+	}
+}
